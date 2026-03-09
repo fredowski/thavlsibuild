8c0fddd84196e5463fa32810c22e0f8a9fce5828
Friedrich Beckmann <friedrich.beckmann@tha.de>
2026-03-09 15:50:23 +0100

    bazel to bazelisk

--- installdependencies.sh
+++ installdependencies.sh
@@ -13,13 +13,34 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # openroad
 
-## bazel and bazelisk
-sudo apt install apt-transport-https curl gnupg -y
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
-sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+## bazelisk
 
-sudo apt update && sudo apt install bazel
+arch=$(uname -m)
+os=$(uname -s)
+
+case "$os" in
+  Linux)
+    case "$arch" in
+	  x86_64)
+        wget -O baz.deb https://github.com/bazelbuild/bazelisk/releases/download/v1.28.1/bazelisk-amd64.deb
+        ;;
+	  aarch64)
+        wget -O baz.deb https://github.com/bazelbuild/bazelisk/releases/download/v1.28.1/bazelisk-arm64.deb
+        ;;
+      *)
+        echo "Unknown architecture"
+        exit 1
+        ;;
+    esac
+	;;
+  Darwin)
+    echo "Todo"
+    exit  1
+    ;;
+esac
+
+sudo dpkg -i baz.deb
+rm baz.deb
 
 ## Some xcb and friends libraries on aarch64
 sudo apt -y install libX11-dev libice-dev libxcb-cursor-dev libxcb-iccm4-dev \
