edc714fb325fcb2411717b43f92790efc567ae90
Friedrich Beckmann <friedrich.beckmann@tha.de>
2026-03-18 21:24:04 +0100

    build.sh: change install path to /opt/openroad

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Copyright Friedrich Beckmann, 2026
 
 # This one overwrites the default settings in the tools
-#export PREFIX=/home/caeuser/inst
+export PREFIX=/opt/openroad
 
 # The versions are based on the IIC-OSIC-TOOLS 2026.02
 # https://github.com/iic-jku/IIC-OSIC-TOOLS/blob/2026.02/_build/tool_metadata.yml
