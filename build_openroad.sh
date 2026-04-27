e77724c1029c757f64807fde7855250fc9889a04
Friedrich Beckmann <friedrich.beckmann@tha.de>
2026-04-27 18:05:48 +0200

    openroad: update version (git 27.4.2026) due to wrong timing path colors
    The timing path colors are all black. With the newer version the
    timing path components are shown in the correct color.

--- build_openroad.sh
+++ build_openroad.sh
@@ -5,7 +5,7 @@
 : ${PREFIX:=/home/caeuser/install}
 
 
-OPENROADCOMMIT=d807840d5c18ee8c7116bb6775c8aa915e1a3844
+OPENROADCOMMIT=e6afe2abf125e5ea0f8a33572a54dd214a6f6397
 
 # openroad
 if [ ! -d OpenROAD ]; then
@@ -18,6 +18,7 @@ git submodule update
 bazelisk build --//:platform=gui //:openroad 
 
 # Hack until PR https://github.com/The-OpenROAD-Project/OpenROAD/pull/9656 is merged
+mkdir -p $PREFIX/bin
 cp -f bazel-bin/openroad $PREFIX/bin/
 cp -f -r -L bazel-bin/openroad.runfiles $PREFIX/bin/
 cd $PREFIX/bin
